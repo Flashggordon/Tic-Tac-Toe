2176dc7120c626ce79334e97c6904f2ee32783b2
George Gordon <georgegordon@Georges-MBP.lan>
2023-12-15 10:22:22 +0000

    first 3 squares created

---
+++ game/src/App.js
@@ -0,0 +1,11 @@
+export default function Square() {
+  return (
+  <>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+  
+  </>
+  );
+
+}
